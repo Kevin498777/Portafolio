198e593bd90f9e9a26a40b21f4652992ac941fbf
Kevin <kevinbrianip498@gmail.com>
2026-06-15 21:06:52 -0600

    Segunda version de las tecnologias

--- core/views.py
+++ core/views.py
@@ -8,10 +8,15 @@ def home(request):
     proyectos_destacados = Proyecto.objects.filter(destacado=True)[:3]
     habilidades = Habilidad.objects.all()
 
-    habilidades_por_categoria = {}
+    habilidades_por_categoria_raw = {}
     for h in habilidades:
         cat = h.get_categoria_display()
-        habilidades_por_categoria.setdefault(cat, []).append(h)
+        habilidades_por_categoria_raw.setdefault(cat, []).append(h)
+
+    # Ordenar categorías: más habilidades primero
+    habilidades_por_categoria = dict(
+        sorted(habilidades_por_categoria_raw.items(), key=lambda x: len(x[1]), reverse=True)
+    )
 
     return render(request, "core/home.html", {
         "proyectos": proyectos_destacados,
